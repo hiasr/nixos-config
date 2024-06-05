76dc4a615265b6673168fdea3504f07fa0bb131c
Ruben Hias <ruben.hias@gmail.com>
2024-06-05 12:21:37 +0200

    Add secureboot

---
+++ nixos/secureboot.nix
@@ -0,0 +1,15 @@
+{ inputs, outputs, lib, config, pkgs, ...}: {
+    imports = [
+        inputs.lanzaboote.nixosModules.lanzaboote
+    ]
+
+    environment.systemPackages = [
+        pkgs.sbctl
+    ];
+
+    boot.loader.systemd-boot.enable = lib.mkForce false;
+    boot.lanzaboote = {
+        enable = true;
+        pkiBundle = "/etc/secureboot";
+    };
+}
